cf867086672893391c8aa9a177c38566e1d3f568
AribYadi <aribyadi@gmail.com>
2025-03-01 18:03:27 +0700

    b

--- src/java.base/share/classes/jdk/internal/org/objectweb/asm/Type.java
+++ src/java.base/share/classes/jdk/internal/org/objectweb/asm/Type.java
@@ -763,7 +763,7 @@ public final class Type {
         System.out.println(methodDescriptor);
         // Parse the argument types and compute their size, one at a each loop iteration.
         while (currentChar != ')') {
-            System.out.println("CurrentOutput Before: " + CurrentOutput);
+            System.out.println("CurrentOutput Before: " + currentOutput);
             if (currentChar == 'J' || currentChar == 'D') {
                 currentOffset++;
                 argumentsSize += 2;
@@ -782,7 +782,7 @@ public final class Type {
                 argumentsSize += 1;
             }
             currentChar = methodDescriptor.charAt(currentOffset);
-            System.out.println("CurrentOutput After: " + CurrentOutput);
+            System.out.println("CurrentOutput After: " + currentOutput);
         }
         currentChar = methodDescriptor.charAt(currentOffset + 1);
         if (currentChar == 'V') {
